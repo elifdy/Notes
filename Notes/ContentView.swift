f529554ffab4dc8ab06b6e8611d74804ff0ed4c2
Elif Dede Yildirim <elifdy@auburn.edu>
2024-04-02 20:29:15 -0400

    Initial Commit

---
+++ Notes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Elif Dede on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Elif Dede on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
